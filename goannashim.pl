8746e4e61c5509a9ed8c22397d17fa4f71a881e1
Michael Gatto <mgatto@lisantra.com>
2013-09-06 12:40:32 -0700

    Format code better.

--- goannashim.pl
+++ goannashim.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
  
 use strict;
 use warnings;
@@ -28,7 +28,6 @@ my $bypass_prg_check;
 my $error;
 my $EXPECT;
 my $submit;
-
 
 ##Below are all of the evidence codes which GOanna supports. They must be passed explicitly.
 
@@ -82,104 +81,100 @@ my @hcontent;
 my $ua = LWP::UserAgent->new;
 
 # open output file
-
 open (my $OUT, '>', "GOanna_out.html");
 
 my $file = join("", $ID_LIST);
 
 my $req = (POST 'http://agbase.hpc.msstate.edu/cgi-bin/tools/GOanna.cgi',
-		Content_Type => 'multipart/form-data',
-		Content	  => [ 'file_type' => "fasta",
-						'PROGRAM' => $PROGRAM,
+    Content_Type => 'multipart/form-data',
+	Content => [
+        'file_type' => "fasta",
+		'PROGRAM' => $PROGRAM,
+        'EMAIL' => $EMAIL,
+        'file_type' =>  $file_type,
+        'DATABASE' => $DATABASE,
+        'DATABASE' => $DATABASE2,
+        'DATABASE' => $DATABASE3,
+        'MATRIX_NAME' => 'BLOSUM62',
+        'search_field' => 'Select ID',
+        'EXPECT' => $EXPECT,
+        'no_iea' => $no_iea,
+        'IEA' => $IEA,
+        'ISO' => $ISO,
+        'EXP' => $EXP,
+        'IBA' => $IBA,
+        'IBD' => $IBD,
+        'IC' => $IC,
+        'IDA' => $IDA,
+        'IEP' => $IEP,
+        'IGC' => $IGC,
+        'IGI' => $IGI,
+        'IKR' => $IKR,
+        'IMP' => $IMP,
+        'IPI' => $IPI,
+        'IRD' => $IRD,
+        'ISA' => $ISA,
+        'ISM' => $ISM,
+        'ISO' => $ISO,
+        'ISS' => $ISS,
+        'NAS' => $NAS,
+        'ND' => $ND,
+        'NR' => $NR,
+        'RCA' => $RCA,
+        'TAS' => $TAS,
+        'WORD_SIZE' => $WORD_SIZE,
+        'DESCRIPTIONS' => $DESCRIPTIONS,
+        'ALIGNMENTS' => $ALIGNMENTS,
+        'submit' => 'BLAST',
+        'GAP_COSTS' => 'Existence: 11 Extension: 1',
+        'IDLIST' => ["$file"] ,
+        'error' => $error,
+        'bypass_prg_check' => $bypass_prg_check
+	]
+);
 
-						'EMAIL' => $EMAIL,
-						'file_type' =>  $file_type,
-						'DATABASE' => $DATABASE,
-						'DATABASE' => $DATABASE2,
-						'DATABASE' => $DATABASE3,
-						'MATRIX_NAME' => 'BLOSUM62',
-						'search_field' => 'Select ID',
-						'EXPECT' => $EXPECT,
-						'no_iea' => $no_iea,
-						'IEA' => $IEA,
-						'ISO' => $ISO,
-						'EXP' => $EXP,
-						'IBA' => $IBA,
-						'IBD' => $IBD,
-						'IC' => $IC,
-						'IDA' => $IDA,
-						'IEP' => $IEP,
-						'IGC' => $IGC,
-						'IGI' => $IGI,
-						'IKR' => $IKR,
-						'IMP' => $IMP,
-						'IPI' => $IPI,
-						'IRD' => $IRD,
-						'ISA' => $ISA,
-						'ISM' => $ISM,
-						'ISO' => $ISO,
-						'ISS' => $ISS,
-						'NAS' => $NAS,
-						'ND' => $ND,
-						'NR' => $NR,
-						'RCA' => $RCA,
-						'TAS' => $TAS,
-						'WORD_SIZE' => $WORD_SIZE,
-						'DESCRIPTIONS' => $DESCRIPTIONS,
-						'ALIGNMENTS' => $ALIGNMENTS,
-						'submit' => 'BLAST',
-						'GAP_COSTS' => 'Existence: 11 Extension: 1',
-						'IDLIST' => ["$file"] ,
-						'error' => $error,
-						'bypass_prg_check' => $bypass_prg_check
+$request = $ua->request($req);
+$content = $request->content;
 
-					   ]
-			  );
- $request = $ua->request($req);
- $content = $request->content;
 my $job_id;
-   print $OUT $content;
-	#$content =~ /job_id\=([a-z0-9]+$)/i;
-	$content =~ /job_id\:\s(\S{6}\S{10})/i;
-	my $job_id = $1;
-	my $req_two;
-	my $out_url = join( "", "http://www.agbase.msstate.edu/tmp/GOAL/", $job_id, '.zip');
-	my $out_filename = join( "", $job_id, ".zip");
-	#-- fetch the zip and save it as perlhowto.zip
-	my $status = getstore($out_url, $out_filename);
+print $OUT $content;
+
+$content =~ /job_id\:\s(\S{6}\S{10})/i;
+my $job_id = $1;
+my $req_two;
+my $out_url = join( "", "http://www.agbase.msstate.edu/tmp/GOAL/", $job_id, '.zip');
+my $out_filename = join( "", $job_id, ".zip");
+
+#-- fetch the zip and save it as perlhowto.zip
+my $status = getstore($out_url, $out_filename);
 my $maxwait = 1200;
 my $initwait = 10;
 my $totalwait = 0;
-if (defined $job_id){
-
-	until ( is_success($status) ){
+if (defined $job_id) {
+	until ( is_success($status) ) {
 		print "Error downloading file: $status\n";
 		print "Trying again in $initwait seconds\n";
 		sleep($initwait);
 		$initwait = $initwait*1.2;
-		if ($initwait > $maxwait){
+		if ($initwait > $maxwait) {
 			$initwait=$maxwait;
-
-}
+        }
 		$totalwait=$totalwait+$initwait;
 		$status = getstore($out_url, $out_filename);
-if ($totalwait > 36000) {
-print "Maximum wait time exceeded; either GOanna is down or the submission will never finish.";
-exit;
-}
-}
-if ( is_success($status) ) {
-			print "The GOanna file downloaded correctly\nIts name is $out_filename\n";
-			sleep(3);
-			unlink "GOanna_out.html";
-			exit;
-			}
-}
 
-else {
-print "The submission failed, likely because of bad parameters\n";
-exit;
+        if ($totalwait > 36000) {
+            print "Maximum wait time exceeded; either GOanna is down or the submission will never finish.";
+            exit;
+        }
+    }
+
+    if ( is_success($status) ) {
+		print "The GOanna file downloaded correctly\nIts name is $out_filename\n";
+        sleep(3);
+        unlink "GOanna_out.html";
+        exit;
+    }
+} else {
+    print "The submission failed, likely because of bad parameters\n";
+    exit;
 }
-
-
-
